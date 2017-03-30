8b7ddae54566012c6436869d8cda043d585cca3b
Axel <axelfuh@gmail.com>
2017-03-30 17:36:44 +0200

    a

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const css = require('./main.sass');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './lib/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import App from './lib/App.jsx';
+
+ReactDOM.render(<App /> , document.getElementById('app'));
